033c7e6f97c53d1babe1fc87dcf698af6c6dd28a
buzenga <100876928+buzenga@users.noreply.github.com>
2022-03-20 23:11:37 +0100

    Add new features (one audio at time)
    Update in js code allowed to play only one audio at a time. The images of animals do not stack one over another anymore.

--- script.js
+++ script.js
@@ -6,17 +6,43 @@ window.addEventListener('keydown', e => {
    );
    const photo = document.querySelector('.photo');
 
+   photo.setAttribute("data-key", `${e.keyCode}`);
+   console.log(photo);
+
+   const sounds = document.querySelectorAll('audio');
+
    if(!audio) return;
-   audio.currentTime = 0;
+   sounds.forEach(audio => {
+      audio.pause();
+      audio.currentTime = 0;
+   });
    audio.play();
    animal.classList.add('playing');
 
-   if (e.keyCode == "49") photo.classList.add('chicken');
-   if (e.keyCode == "50") photo.classList.add('cow');
-   if (e.keyCode == "51") photo.classList.add('elephant');
-   if (e.keyCode == "52") photo.classList.add('gorilla');
-   if (e.keyCode == "53") photo.classList.add('pig');
-   if (e.keyCode == "54") photo.classList.add('wolf');
+   if (e.keyCode == "49") {
+      photo.classList.remove('chicken','cow','elephant','gorilla','pig','wolf');
+      photo.classList.add('chicken');
+   }
+   if (e.keyCode == "50") {
+      photo.classList.remove('chicken','cow','elephant','gorilla','pig','wolf');
+      photo.classList.add('cow');
+   }
+   if (e.keyCode == "51") {
+      photo.classList.remove('chicken','cow','elephant','gorilla','pig','wolf');
+      photo.classList.add('elephant');
+   }
+   if (e.keyCode == "52") {
+      photo.classList.remove('chicken','cow','elephant','gorilla','pig','wolf');
+      photo.classList.add('gorilla');
+   }
+   if (e.keyCode == "53") {
+      photo.classList.remove('chicken','cow','elephant','gorilla','pig','wolf');
+      photo.classList.add('pig');
+   }
+   if (e.keyCode == "54") {
+      photo.classList.remove('chicken','cow','elephant','gorilla','pig','wolf');
+   photo.classList.add('wolf');
+   }
 
    audio.addEventListener('ended', () => {
    photo.classList.remove('chicken');
